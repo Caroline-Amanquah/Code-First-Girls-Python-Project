76b80b218ced921a9e13056dca579a577e9427c1
Caroline Amanquah <carolineamanquah@gmail.com>
2022-07-14 20:46:47 +0100

    Add files via upload
    Code First Girls Project March 2022

---
+++ Analysisofcsvfile.py
@@ -0,0 +1,21 @@
+import csv
+filename = 'sales.csv'
+
+with open('sales.csv','r') as csv_file:
+    csvReader=csv.reader(csv_file)
+    for row in csvReader:
+        print(row)
+
+sales = [6226, 1521,  1842, 2051, 1728, 2138, 7479, 4434, 3615, 5472, 7224, 1812]
+total = sum(sales)
+print("The total of all sales is: £",sum(sales))
+with open('sales.csv','r') as csv_file:
+    csvReader=csv.reader(csv_file)
+
+    next(csvReader)
+
+    for line in csvReader:
+        sales.append
+
+    print(sales)
+    print(type(sales))
